534f08e14454adf36597d31efee4c993a8fb758f
nkkunzler <nkkunzler@gmail.com>
2019-01-18 22:33:09 -0700

    Added comments to ExcelSheet.java

--- src/main/ExcelSheet.java
+++ src/main/ExcelSheet.java
@@ -32,15 +32,19 @@ public class ExcelSheet {
 		workbook = new XSSFWorkbook();
 		sheet = workbook.createSheet(sheetName);
 
+		// Creating a new row for the header text
 		Row header = sheet.createRow(0);
 
 		Object[] keySet = columnValues.keySet().toArray();
 
+		// If there are headers display on the header row
 		if (showHeader) {
 			for (int i = 0; i < keySet.length; ++i)
+				// Creating a new cell to store the headers in each cell
 				createCell(header, i, keySet[i].toString());
 		}
-		
+
+		// Need to limit the number of columns, to be print friendly
 		for (int i = 0; i < keySet.length; ++i) {
 			int currRow = showHeader ? 1 : 0;
 			for (int j = 0; j < columnValues.get(keySet[i]).size(); ++j) {
@@ -50,22 +54,17 @@ public class ExcelSheet {
 				createCell(row, i, columnValues.get(keySet[i]).get(j));
 			}
 		}
-		
-		double val = Math.random() * 5;
-		String fileName = "test.out";
-	    FileWriter fileWriter = null;
-		try {
-			fileWriter = new FileWriter(fileName);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    PrintWriter printWriter = new PrintWriter(fileWriter);
-	    printWriter.print("Some String");
-	    printWriter.printf("Product name is %s and its price is %d $", "iPhone", 1000);
-	    printWriter.close();
 	}
 
+	/**
+	 * Creates a new Excel cell at the specified row and column. This Excel cell
+	 * is a standard Excel set width with thick borders.
+	 * 
+	 * @param row       Integer representing the row at which to create the cell
+	 * @param column    Integer representing the column at which to create the
+	 *                  cell
+	 * @param cellValue The string value that will be placed within the cell
+	 */
 	private void createCell(Row row, int column, String cellValue) {
 		CellStyle style = workbook.createCellStyle();
 		BorderStyle borderStyle = BorderStyle.THICK;
@@ -79,11 +78,25 @@ public class ExcelSheet {
 		cell.setCellValue(cellValue);
 	}
 
+	/**
+	 * Using a map which resizes the column width,specified by the key of map,
+	 * to the desired width, the value.
+	 * 
+	 * @param columnWidths Map<Integer, Integer> that maps the column to the
+	 *                     desired width
+	 */
 	public void setColumnWidths(Map<Integer, Integer> columnWidths) {
 		for (Integer col : columnWidths.keySet())
 			sheet.setColumnWidth(col, columnWidths.get(col));
 	}
 
+	/**
+	 * Using the desired path and file name, this method saves the current state
+	 * of the ExcelSheet as an .xlsx, or Excel, file.
+	 * 
+	 * @param path     The desired file path for the file
+	 * @param fileName The name of the file to save.
+	 */
 	public void save(String path, String fileName) {
 		String fileLocation = path + fileName + ".xlsx";
 
@@ -94,10 +107,12 @@ public class ExcelSheet {
 			new Popup(AlertType.ERROR, "Invalid file path provided.");
 		}
 
+		// Checking to make sure the file location is valid
 		if (outputStream == null) {
 			new Popup(AlertType.ERROR, "File was unable to be created.");
 		}
 
+		// Saving the workbook to the specified file location
 		try {
 			workbook.write(outputStream);
 			workbook.close();
